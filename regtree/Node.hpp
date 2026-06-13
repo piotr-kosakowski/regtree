b2ffae7057cd31088c85deb264043dff986d4a93
piotr-kosakowski <piotrkosakowski@icloud.com>
2026-06-13 13:46:51 +0200

    add doxygen tag comments in .hpp files

--- regtree/Node.hpp
+++ regtree/Node.hpp
@@ -2,19 +2,32 @@
 #define NODE_HPP
 
 #include <memory>
-
+/**
+ * @brief Represent a single node in regression tree.
+ * 
+ * A node can be either an internal one with a split rule
+ * or a leaf with a predicted value. 
+ */
 
 struct Node {
+    // Split information for internal nodes
     int feature_index = -1;
     double threshold = 0.0;
     
+    // Leaf information
     bool is_leaf = false;
     double predicted_value = 0.0;
     
+    // Pointers to child nodes for values < threshold (left) and > threshold (right)
     std::unique_ptr<Node> left;
     std::unique_ptr<Node> right;
 
+    /** @brief Default constructor for internal nodes. */
     Node() = default;
+    /**
+     * @brief Constructor for leaf nodes.
+     * @param val The constant value this leaf will predict.
+     */
     explicit Node(double val) : is_leaf(true), predicted_value(val) {}
 };
 
--- regtree/RegressionTree.hpp
+++ regtree/RegressionTree.hpp
@@ -5,36 +5,62 @@
 #include <vector>
 #include <memory>
 
+/**
+ * @brief Represents whole regression Tree for numercial features.
+ * 
+ * This class implements a greedy recursive algortihm that
+ * minimizes SSE at each split.
+ */
 class RegressionTree {
 public:
+    /**
+     * @brief Constructor for a new Regression Tree object.
+     * @param max_depth Maximum depth of tree (0 = root only). Must be < 32.
+     * @param min_samples_split  Minimum samples required to attempt a split.
+     */
     RegressionTree(int max_depth = 10, int min_samples_split = 2) 
         : max_depth(max_depth), min_samples_split(min_samples_split) {}
     
     int max_depth;
     int min_samples_split;
-
+    /**
+     * @brief  Fits the tree to a training dataset.
+     * @param X Flat, C-contigious 1D array of features (size: n_samples * n_features). 
+     * @param y Flat, C-contigious 1D array of target va;ues (size: n_samples).
+     * @param n_samples Number of samples (rows).
+     * @param n_features Number of features (columns).
+     */
     void fit(const double* X, const double* y, int n_samples, int n_features);
 
+    /**
+    * @brief Predicts target values for multiple samples.
+    * @param X Flat 1D array of features to predict.
+    * @param out Pointer to the array where predictions will be stored.
+    * @param n_samples Number of samples to predict.
+    */
     void predict(const double* X, double* out, int n_samples) const;
 
 private:
     std::unique_ptr<Node> root_;
-    std::vector<int> indices;
+    std::vector<int> indices; // Bitmask vector used for in-place partitioning.
     int n_features_ = 0;
 
+    /** @brief Traverses the tree for a single sample and returns final leaf's value. */
     double predict_one(const double* x) const;
 
+    /** @brief Internal recursive function to build the tree. */
     std::unique_ptr<Node> build_tree(const double* X, 
                                      const double* y, 
                                      int depth,
                                      int n_samples_split,
                                      int mask);
-
+    /** @brief Helper structure to store the best split found. */
     struct Split {
         int feature_index = -1;
         double threshold = 0.0;
     };
 
+    /**@brief Checks possible splits to find the one, that minimizes SSE */
     Split find_best_split(const double* X, 
                           const double* y,
                           int mask);
