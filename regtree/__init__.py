337a1a928cc0e89ffa53d70ee493644dcaaa3ed9
piotr-kosakowski <piotrkosakowski@icloud.com>
2026-06-13 13:16:04 +0200

    update __init__

--- regtree/__init__.py
+++ regtree/__init__.py
@@ -7,9 +7,17 @@ class DecisionTreeRegressor:
         self.tree = PyRegressionTree(self.max_depth, self.min_samples_split)
 
     def fit(self, X, y):
-        self.tree.fit(X[::1], y[::1], X.shape[0], X.shape[1])
+        assert X.ndim == 2, "X must be a 2D array"
+        assert y.ndim == 1, "y must be a 1D array"
+        assert X.shape[0] == y.shape[0], "Number of samples in X and y must be equal"
+        assert X.flags['C_CONTIGUOUS'], "X must be a C-contiguous array"
+        assert y.flags['C_CONTIGUOUS'], "y must be a C-contiguous array"
+        self.tree.fit(X[::1].ravel(), y[::1], X.shape[0], X.shape[1])
 
     def predict(self, X):
+        assert X.ndim == 2, "X must be a 2D array"
+        assert X.shape[0] == y.shape[0], "Number of samples in X and y must be equal"
+        assert X.flags['C_CONTIGUOUS'], "X must be a C-contiguous array"
         if self.tree is None:
             raise ValueError("The model has not been fitted yet.")
-        return self.tree.predict(X[::1], X.shape[0])
+        return self.tree.predict(X[::1].ravel(), X.shape[0])
